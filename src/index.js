f0797b35c4a1a711bcd665e7d916e410d67664f2
Betsy Tsoi <tsoisybetsy@gmail.com>
2022-12-29 01:15:58 +0900

    Build MVP

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import generateHomepage from './generateDOM';
+import getCurrentWeather from './weatherApi';
+import displayCurrentWeather from './displayDOM';
 import './styles.css';
+
+generateHomepage();
+getCurrentWeather('Tokyo')
+  .then((data) => displayCurrentWeather(data));
