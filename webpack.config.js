2b3b67952acff6a57deb61eea3812ffa32e97ca5
Betsy Tsoi <tsoisybetsy@gmail.com>
2023-01-17 15:10:45 +0900

    For Github Page

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
   ],
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'gh-pages'),
+    path: path.resolve(__dirname, 'docs'),
     clean: true,
   },
   module: {
